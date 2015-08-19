65b665b549459ec45c3fe3351b90f6ccb364d0fe
Tom Parker <tparker@usgs.gov>
2015-08-18 19:10:47 -0800

    init

---
+++ src/main/java/gov/usgs/volcanoes/logger2csv/App.java
@@ -0,0 +1,13 @@
+package gov.usgs.volcanoes.logger2csv;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
