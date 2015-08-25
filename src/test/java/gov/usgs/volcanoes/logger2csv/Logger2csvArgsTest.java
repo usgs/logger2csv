497175ada929e71c8acc2ddcd6ef094d3554195e
Tom Parker <tparker@usgs.gov>
2015-08-25 12:23:35 -0800

    Fix test class name

--- src/test/java/gov/usgs/volcanoes/logger2csv/Logger2csvArgsTest.java
+++ src/test/java/gov/usgs/volcanoes/logger2csv/Logger2csvArgsTest.java
@@ -9,7 +9,7 @@ import org.junit.Test;
 import com.martiansoftware.jsap.JSAPException;
 
 
-public class PensiveArgsTest {
+public class Logger2csvArgsTest {
 
     private static final String CONFIG_FILENAME = "test.config";
     
