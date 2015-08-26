6d52746ca791f353fb5fc3a5006aebd204849f60
Tom Parker <tparker@usgs.gov>
2015-08-25 22:24:56 -0800

    Migrate to decorator args

--- src/main/java/gov/usgs/volcanoes/logger2csv/Logger2csvArgs.java
+++ src/main/java/gov/usgs/volcanoes/logger2csv/Logger2csvArgs.java
@@ -1,11 +1,18 @@
 package gov.usgs.volcanoes.logger2csv;
 
-import com.martiansoftware.jsap.JSAP;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.martiansoftware.jsap.JSAPException;
+import com.martiansoftware.jsap.JSAPResult;
 import com.martiansoftware.jsap.Parameter;
+import com.martiansoftware.jsap.ParseException;
 import com.martiansoftware.jsap.Switch;
-import com.martiansoftware.jsap.UnflaggedOption;
 
 import gov.usgs.volcanoes.util.args.Args;
+import gov.usgs.volcanoes.util.args.Arguments;
+import gov.usgs.volcanoes.util.args.decorator.ConfigFileArg;
+import gov.usgs.volcanoes.util.args.decorator.CreateConfigArg;
 
 /**
  * Argument processor for Logger2csv
@@ -16,27 +23,42 @@ import gov.usgs.volcanoes.util.args.Args;
  *         through the CC0 1.0 Universal public domain dedication.
  *         https://creativecommons.org/publicdomain/zero/1.0/legalcode
  */
-public class Logger2csvArgs extends Args {
-    
-    public static final String EXAMPLE_CONFIG_FILENAME = "logger2csv-example.config";
-    public static final String DEFAULT_CONFIG_FILENAME = "logger2csv.config";
-    public static final String PROGRAM_NAME = "java -jar gov.usgs.volcanes.logger2csv.Logger2csv";
-    public static final String EXPLANATION = "I am the logger2csv server\n";
+public class Logger2csvArgs {
+	private static final Logger LOGGER = LoggerFactory.getLogger(Logger2csvArgs.class);
 
-    private static final Parameter[] PARAMETERS = new Parameter[] {
-            new Switch("persistent", 'p', "persistent", "Run persistenly, periodically polling loggers."),
-            new Switch("verbose", 'v', "verbose", "Verbose logging.") 
-            };
+	public static final String EXAMPLE_CONFIG_FILENAME = "logger2csv-example.config";
+	public static final String DEFAULT_CONFIG_FILENAME = "logger2csv.config";
+	public static final String PROGRAM_NAME = "java -jar gov.usgs.volcanes.logger2csv.Logger2csv";
+	public static final String EXPLANATION = "I am the logger2csv server\n";
 
-    public final boolean verbose;
-    public final boolean persistent;
+	private static final Parameter[] PARAMETERS = new Parameter[] {
+			new Switch("persistent", 'p', "persistent", "Run persistenly, periodically polling loggers."), };
 
-    public Logger2csvArgs(String[] args) {
-        super(PROGRAM_NAME, EXPLANATION, PARAMETERS);
-        addCreateConfig(EXAMPLE_CONFIG_FILENAME, DEFAULT_CONFIG_FILENAME);
-        config = parse(args);
-        
-        verbose = config.getBoolean("verbose");
-        persistent = config.getBoolean("persistent");
-    }
+	public final boolean persistent;
+	public final String configFileName;
+
+	public Logger2csvArgs(String[] commandLineArgs) {
+		Arguments args = new Args(PROGRAM_NAME, EXPLANATION, PARAMETERS);
+		try {
+			args = new ConfigFileArg(DEFAULT_CONFIG_FILENAME, args);
+			args = new CreateConfigArg(EXAMPLE_CONFIG_FILENAME, args);
+		} catch (JSAPException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+		JSAPResult jsapResult = null;
+		try {
+			jsapResult = args.parse(commandLineArgs);
+		} catch (ParseException e) {
+			LOGGER.error("Cannot parse command line.");
+			System.exit(1);
+		}
+
+		persistent = jsapResult.getBoolean("persistent");
+        LOGGER.debug("Setting: persistent={}", persistent);
+
+        configFileName = jsapResult.getString("config-filename");
+        LOGGER.debug("Setting: configFileName={}", configFileName);
+	}
 }
--- src/main/java/gov/usgs/volcanoes/logger2csv/Logger2csvVersion.java
+++ src/main/java/gov/usgs/volcanoes/logger2csv/Logger2csvVersion.java
@@ -2,7 +2,7 @@ package gov.usgs.volcanoes.logger2csv;
 
 public final class Logger2csvVersion {
 
-    public static final String BUILD_TIME="2015-08-26T00:13:21Z";
+    public static final String BUILD_TIME="2015-08-26T05:47:39Z";
 
     public static final String POM_VERSION="1.0.1";
 
