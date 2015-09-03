66a9f62e6491754608035435fd658ce7725d6dee
Tom Parker <tparker@usgs.gov>
2015-09-03 08:55:51 -0800

    Add issueManagement stanza to pom

--- src/main/java/gov/usgs/volcanoes/logger2csv/Logger2csvVersion.java
+++ src/main/java/gov/usgs/volcanoes/logger2csv/Logger2csvVersion.java
@@ -2,7 +2,7 @@ package gov.usgs.volcanoes.logger2csv;
 
 public final class Logger2csvVersion {
 
-    public static final String BUILD_TIME="2015-09-01T18:37:18Z";
+    public static final String BUILD_TIME="2015-09-02T21:18:35Z";
 
     public static final String POM_VERSION="1.0.1";
 
