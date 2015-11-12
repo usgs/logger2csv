940e12bf958ab079e6ed8deb33445b45a441b3f2
Tom Parker <tparker@usgs.gov>
2015-11-12 09:07:51 -0900

    Version bump and README update.

--- src/main/java/gov/usgs/volcanoes/logger2csv/Logger2csvVersion.java
+++ src/main/java/gov/usgs/volcanoes/logger2csv/Logger2csvVersion.java
@@ -2,9 +2,9 @@ package gov.usgs.volcanoes.logger2csv;
 
 public final class Logger2csvVersion {
 
-    public static final String BUILD_TIME="2015-09-03T18:44:37Z";
+    public static final String BUILD_TIME="2015-11-12T17:41:15Z";
 
-    public static final String POM_VERSION="1.0.1";
+    public static final String POM_VERSION="2.0.0-SNAPSHOT";
 
 	public static final String VERSION_STRING = "Version: " + POM_VERSION + " Built: " + BUILD_TIME;
 }
